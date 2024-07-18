4b81047ca443f21bff1b5761bfe738b905c6dc49
Shmuel Kulikin <shmuel.kulikin@targatelematics.com>
2024-07-18 11:08:08 +0300

    add package swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "mobileshared",
+    products: [
+        .library(
+            name: "mobileshared",
+            targets: ["mobileshared"])
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+			name: "mobileshared",
+			url: "https://github.com/Driveway/MobileShared-Releases/releases/download/12.7.0/mobileshared.zip",
+			checksum: "fe3647812991fe6deb3486e1e028bbbbb0bc89a19584c66d8a0deb2092a18493")
+    ]
+)
