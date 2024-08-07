77d0066329f43c92cad43d262b09a9db3a3a329f
Evgeny Pistunovich <evgeny.pistunovich@targatelematics.com>
2024-08-07 16:18:06 +0300

    Update binaryTarget with new URL and checksum

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
 			name: "mobileshared",
-			url: "https://github.com/Driveway/DriveIt-MobileShared-Releases/releases/download/12.7/mobileshared.zip",
+			url: "https://github.com/Driveway/DriveIt-MobileShared-Releases/releases/download/12.7.20/mobileshared.zip",
 			checksum: "26d8d6d57ebe7ca0a7bd0488918d191a4e7864b78fa9fcda186e7a28dc6f54f1")
     ]
 )
